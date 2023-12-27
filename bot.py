6216421574e630b86e425c5455747c76a9052312
Amir Sanchouli <amir.sanchouli00@gmail.com>
2023-12-28 02:12:54 +1100

    (ADD) Added comprehensive documentation

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import telebot
 import downloader_script as ds
 import configobj
 
-#TODO:
+# TODO:
 # 1. Add telegram "hinting" for commands
 # 2. Exception and error handling (иначе всё нахуй упадет на облаке/сервакe)
 # 3. Integration with Selenium to be able to download age restricted videos (for music lol)
@@ -22,38 +22,78 @@ Hi there! I am Downloader test bot, Dev version.
 Just a kind words of greetings before we start testing!\
 """
 
+
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
+    """
+    Function description: Sends welcome message upon "\start" command.
+    Approach description:
+    """
     bot.reply_to(message, WELCOME_MSG)
 
 
 @bot.message_handler(commands=['help'])
 def send_help_message(message):
+    """
+    Function description: Sends message with guide and instructions upon "\help" command.
+    Approach description:
+    """
     bot.send_message(message.chat.id, HELP)
 
 
-#Handle '/download' command
+# Handle '/download' command
 @bot.message_handler(commands=['download'])
 def download_audio(message):
+    """
+    Function description: This function responds to "\downlaad" command and triggers get_audio_only()
+    function from ./downloader_script.py and sends converted mp3 file in Telegram chat.
+
+    Approach description:  ...
+
+    Input:
+        message: text message in form: "/download {youtube_video_url}"
+    Output: None
+    """
+
+    # split input text (i.e "/download {youtube_video_url}")
     video_url = message.text.split()[1]
+
+    # convert video and get file name
     file_name = ds.get_audio_only(video_url, "./downloaded")
     print(str(file_name))
 
     audio_path = f'./downloaded/{file_name}'
+
     # audio_path = f'./downloaded/{file_name}'
     # bot.send_audio(chat_id=message.chat.id ,audio=open(f'./downloaded/{file_name}', encoding='cp850'))
+
+    # open audio with appropriate encoding
     audio_file = open(audio_path, 'rb')
+
+    # send output file to user
     bot.send_audio(chat_id=message.chat.id, audio=audio_file)
 
-    # seems like shit crashes when I have something like this:
+    # seems like script crashes when I have something like this:
     # "𝕋𝕙𝕖 𝕃𝕠𝕤𝕥 𝕊𝕠𝕦𝕝 𝔻𝕠𝕨𝕟 𝕩 𝕃𝕠𝕤𝕥 𝕊𝕠𝕦𝕝 - ℕ𝔹𝕊ℙ𝕃𝕍 [ ℂ𝕙𝕒𝕚𝕟𝕤𝕒𝕨 𝕄𝕒𝕟 𝔾𝕚𝕣𝕝𝕤 // 𝟙 ℍ𝕠𝕦𝕣 ℂ𝕝𝕖𝕒𝕟 𝕃𝕠𝕠𝕡 ]"
     # I need to find a way to convert it to regular string without fancy bs. Oh wait, nvm.
     # Seems like vid itself is 1 hour long
-    # Other thing I gotta consider is deleting video when it failed to send it via tg. That makes sense.
+    # Other thing I have to consider is deleting video when it failed to send it via telegramg. That makes sense.
+    # Telegram bot file limit is 10 MB.
+
 
 @bot.message_handler(commands=['playlist'])
 def download_playlist(message):
+    """
+    Function description: This function downloads an entire playlist
+    provided by user and sends back audi of each track
+
+    Approach description:
+
+    Input:
+        message: text message in form: "/download {youtube_playlist_url}"
+    Output:
+    """
     playlist_url = message.text.split()[1]
     ds.get_playlist_audio(playlist_url, "./downloaded")
 
--- downloader_script.py
+++ downloader_script.py
@@ -2,16 +2,24 @@ from pytube import *
 from moviepy.editor import *
 import os
 
+
 # TODO: 1. Let user choose a directory where they want to save a file. (Done)
 #       2. Add a dropdown to select a video quality. (irrelevant here, no Tkinter used)
 #       3. Adapt scripts for particular functions. (irrelevant here, no Tkinter used)
 #       4. Redesign and colouring. (irrelevant here, no Tkinter used)
 
-file_name_str = ""
-
-
 def get_playlist_video(link, file_path):
-    """Allows to download entire playlist in the highest quality """
+    """
+    Function description: Allows to download entire video playlist in the highest quality
+    Approach:
+
+    Input:
+        link: playlist url
+        file_path: directory where downloaded file will be stored
+
+    Output:
+        None
+    """
     p = Playlist(link)
     print(f"Downloading {p.title} playlist")
     for video in p.videos:
@@ -20,21 +28,40 @@ def get_playlist_video(link, file_path):
 
 
 def convert_to_audio(title, file_path):
+    """
+    Function description: This function performs conversion of downloaded .mp4 video into .mp3 audio.
+
+    Approach:
+
+    Input:
+        title: video title
+        file_path: directory where downloaded file will be stored
+    Output: .mp3 file name
+    """
+
     # getting rid of the "lost" symbols (apparently got fixed in later PyTube releases),at least "!"(exclamation mark)
+    # in order for title input to match the file name of downloaded video
     symbols_to_delete = [",", ";", "$", ":", "/", "."]
     filename = "".join([i for i in title if i not in symbols_to_delete])
 
-    # conversion to mp3
+    # Create mp3 and mp4 files
     mp4_file = os.path.join(file_path, f"{filename}.mp4")
     mp3_file = os.path.join(file_path, f"{filename}.mp3")
+
+    # store mp3 file name separately for return statement
     file_name_str = f"{filename}.mp3"
 
+    # separate audio from video file
     video_clip = VideoFileClip(mp4_file)
     audio_clip = video_clip.audio
 
+    # write audio to a created earlier mp3 file
     audio_clip.write_audiofile(mp3_file)
+
+    # close all files as soon as transfer is done
     audio_clip.close()
     video_clip.close()
+
     # deleting a video fragment for audio only, later will be included to a separate function
     os.remove(os.path.join(file_path, f"{filename}.mp4"))
 
@@ -43,7 +70,12 @@ def convert_to_audio(title, file_path):
 
 def get_audio_only(link, file_path):
     """
-    This function allows to download and extract an audio track of a particular YouTube video
+    Function description: This function allows to download and extract an audio track of a particular YouTube video
+    Approach:
+
+    Input:
+        link: video url
+        file_path: directory where downloaded file will be stored
     """
     yt = YouTube(link, use_oauth=True, allow_oauth_cache=True)
     # print(yt.streams.filter(file_extension='mp4').first())  # idk what it does but let it hang there for a while
@@ -66,16 +98,36 @@ def get_audio_only(link, file_path):
 
 
 def get_playlist_audio(link, file_path):
+    """
+    Function description:
+    Approach:
+
+    Input:
+        link: playlist url
+        file_path: directory where downloaded file will be stored
+
+    Output:
+    """
     p = Playlist(link)
     for track in p.videos:
         print(track.embed_url)
         track_file = track.streams.filter(progressive=True).get_lowest_resolution()
-        track_file .download(file_path)
+        track_file.download(file_path)
         convert_to_audio(track_file.title, file_path)
 
 
-
 def get_resolutions(link):
+
+    """
+    Function description: This function extracts all possible resolutions of a given video
+    Approach: We filter the streams and keep only ones with mp4 file extension, afterward we
+    iterate through the resultant list and extract unique resolutions through set()
+
+    Input:
+        link: video url
+
+    Output: None
+    """
     yt = YouTube(link)
     filter_stream_list = yt.streams.filter(file_extension='mp4', only_audio=False, type="video")
     resolutions_set = set()
